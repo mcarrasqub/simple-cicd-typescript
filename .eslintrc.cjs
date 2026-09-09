da308ede50566ba9f28e79a639627156256dceb9
mariana <mcarrasquilla323@gmail.com>
2026-09-09 12:25:40 -0500

    calculadora completa

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
   ],
-  ignorePatterns: ["node_modules/", "sum.js"],
+  ignorePatterns: ["node_modules/", "*.js"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-require-imports": "off",
